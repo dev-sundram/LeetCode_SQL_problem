b284cda08214346ca5676e4029829873f4235c85
Sundram Kumar <165772663+dev-sundram@users.noreply.github.com>
2026-09-19 19:34:11 +0530

    Time: 695 ms (75.18%), Space: 0B (100%) - LeetHub

---
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT s.user_id, ROUND(AVG(CASE
+                     When c.action = 'confirmed' THEN 1 ELSE 0 END  ),2) as confirmation_Rate
+FROM Signups s 
+LEFT JOIN Confirmations c
+  ON s.user_id = c.user_id
+GROUP BY s.user_id 
